689974368c9e5ef773db8e3ad5aa9c8b41eef313
Warding4Softwares <warding4softwares@gmail.com>
2024-01-29 11:42:09 +0100

    The Project: 3%

---
+++ Client/src/Components/Login.jsx
@@ -0,0 +1,19 @@
+export default function Login() {
+  return (
+    <div>
+      <form>
+        <img src="/Logo.svg" />
+        <div>
+            <label></label>
+            <input type="email" name="Email" placeholder="Email" />
+        </div>
+        <div>
+            <label></label>
+            <input type="password" />
+        </div>
+        <div></div>
+        <div></div>
+      </form>
+    </div>
+  );
+}
